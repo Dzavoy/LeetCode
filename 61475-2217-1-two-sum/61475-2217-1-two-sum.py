608f7b7ab6a04712a1eec932048ad5cef57a0b34
João Rosa <Dzavoy@proton.me>
2025-05-10 01:07:42 -0300

    Time: 0 ms, Memory: 19.1 MB - LeetHub

--- 61475-2217-1-two-sum/61475-2217-1-two-sum.py
+++ 61475-2217-1-two-sum/61475-2217-1-two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen: dict = {}
+        seen: dict[int, int] = {}
         for i, num in enumerate(nums):
-            diff = target - num
+            diff: int = target - num
             if diff in seen:
                 return [seen[diff], i]
             seen[num] = i
